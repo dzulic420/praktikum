ddb09f5e38cee4e46f65a3265c335edf651ef47e
dzulic420 <63450180+dzulic420@users.noreply.github.com>
2020-05-28 13:45:13 +0200

    Update hotel.js

--- tdd-demo/hotel.js
+++ tdd-demo/hotel.js
@@ -1,9 +1,9 @@
 let pricelist = [
-    { from: "2020-01-01", to: "2020-02-01", price: 34.5 },
-    { from: "2020-02-02", to: "2020-03-01", price: 37.0 },
-    { from: "2020-03-02", to: "2020-05-15", price: 39.0 },
-    { from: "2020-05-16", to: "2020-06-15", price: 37.0 },
-];
+    { from: '2020-01-01', to: '2020-02-01', price: 34.5 },
+    { from: '2020-02-02', to: '2020-03-01', price: 37.0 },
+    { from: '2020-03-02', to: '2020-05-15', price: 39.0 },
+    { from: '2020-05-16', to: '2020-06-15', price: 37.0 },
+]
 
 function logPricelist(pricelist) {
     /*
@@ -15,24 +15,19 @@ function logPricelist(pricelist) {
     
     (sortirano od manje do veće cijene)
     */
-    let ispisi = ""
-    let provjera = {}
-    pricelist.sort((a, b)=>a.price - b.price)
-    pricelist.forEach((lista) => {
-        if (lista.price == provjera.price) {
-            ispisi=ispisi + ', ' +`${lista.from}`+" do "+`${lista.to}`
-        }
-        else {
-            ispisi=ispisi+'\n'+`${lista.price}`+":"+ `${lista.from}`+" do "+`${lista.to}`
-            provjera=lista
-        }
+    pricelist.sort((a, b) => a.price - b.price)
+
+    let print = ""
+    let previous = {}
+
+    pricelist.forEach(function (a) {
+        if (a.price == previous.price) print += ' , '
+        else print += '\n'
+        print += `${a.price} : ${a.from} : ${a.to}`
+        previous = a
     })
-    console.log(ispisi)
+
+    console.log(print)
 }
-logPricelist(pricelist);
-//gtreba ispisati slijedeće:
-/*
-34.5 : 2020-01-01 do 2020-02-01
-37.0 : 2020-02-02 do 2020-03-01 , 2020-05-15 do 2020-06-15
-39.0 : "2020-03-02 do 2020-05-15
-*/
+
+logPricelist(pricelist)
